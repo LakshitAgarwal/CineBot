a4bd62e24c4c2268d8c35a287324fb2556f462fd
Lakshit Agarwal <lakshitcollege@gmail.com>
2024-08-09 15:25:52 +0530

    bug fixed

--- src/Components/BrowseComponents/MoviesCatalouge.jsx
+++ src/Components/BrowseComponents/MoviesCatalouge.jsx
@@ -5,7 +5,7 @@ const MoviesCatalouge = () => {
   const store = useSelector((store) => store.movies);
   // console.log(nowPlayingMovies);
   return (
-    <div className="pt-[40%]">
+    <div className="pt-[40%] pb-12">
       <MovieList movies={store.nowPlayingMovies} title="Now Playing Movies" />
       <MovieList movies={store.popularTVSeries} title="Popular TV Seirs" />
       <MovieList movies={store.upcomingMovies} title="Upcoming Movies" />
