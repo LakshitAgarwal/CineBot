e47c4796ea12b1164710dbbba073e7ebb1774292
Lakshit Agarwal <lakshitcollege@gmail.com>
2024-08-09 16:19:33 +0530

    createBrowserRouter changed to createHashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import Login from "./Components/Login.jsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import Browse from "./Components/Browse.jsx";
 
-const appRouter = createBrowserRouter([
+// Creating the router using createHashRouter
+const appRouter = createHashRouter([
   {
     path: "/",
     element: <App />,
@@ -22,6 +23,7 @@ const appRouter = createBrowserRouter([
   },
 ]);
 
+// Rendering the app with the RouterProvider
 ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={appRouter} />
 );
