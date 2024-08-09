f8a1e42ae38334e722a27694f3702b3705b1dea5
Lakshit Agarwal <lakshitcollege@gmail.com>
2024-08-09 14:53:12 +0530

    ShimmerUi Added

--- src/Components/BrowseComponents/MoviesList.jsx
+++ src/Components/BrowseComponents/MoviesList.jsx
@@ -1,25 +1,29 @@
 import MovieCard from "./MovieCard";
+import ShimmerUi from "../ShimmerUi";
 
 const MovieList = ({ movies, title }) => {
-  // console.log(movies);
   return (
-    <div className="ml-12 my-6">
-      <p className="text-3xl relative z-10 archivo-black-regular mb-8  text-white">
-        {title}
-      </p>
-      <div className="relative z-10 overflow-x-scroll whitespace-nowrap hidding-scrollbar">
-        <div className="inline-flex flex-row-reverse space-x-6">
-          {movies
-            ? movies.map((movie, index) => (
+    <div>
+      {movies ? (
+        <div className="ml-12 my-6">
+          <p className="text-3xl relative z-10 archivo-black-regular mb-8 text-white">
+            {title}
+          </p>
+          <div className="relative z-10 overflow-x-scroll whitespace-nowrap hiding-scrollbar">
+            <div className="inline-flex flex-row-reverse space-x-6">
+              {movies.map((movie, index) => (
                 <MovieCard
                   key={index}
                   movieData={movie}
                   moviePosterPath={movie.poster_path}
                 />
-              ))
-            : null}
+              ))}
+            </div>
+          </div>
         </div>
-      </div>
+      ) : (
+        <ShimmerUi />
+      )}
     </div>
   );
 };
