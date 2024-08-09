f7fa15fe78aeec35b91e4c9820746604dbbeaf5c
Lakshit Agarwal <lakshitcollege@gmail.com>
2024-08-10 02:09:15 +0530

    search issue resolved

--- src/Components/Browse.jsx
+++ src/Components/Browse.jsx
@@ -7,6 +7,8 @@ import useFetchUpcomingMovies from "../hooks/useFetchUpcomingMovies";
 import Hero from "./BrowseComponents/Hero";
 import MoviesCatalouge from "./BrowseComponents/MoviesCatalouge";
 import AiSearch from "./AiSearch";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Browse = () => {
   const store = useSelector((store) => store.eventHandling.mouseclicked);
@@ -23,9 +25,16 @@ const Browse = () => {
         <AiSearch />
       ) : (
         <>
-          <div className="h-full bg-black">
+          <div className="relative h-full bg-black">
             <Hero />
             <MoviesCatalouge />
+            <Link to="/search">
+              <div className="fixed z-50 bottom-4 right-4">
+                <div className="flex items-center justify-center p-3 bg-red-600 rounded-full shadow-lg hover:bg-red-700 transition-all duration-300">
+                  <FaSearch className="text-white text-2xl" />
+                </div>
+              </div>
+            </Link>
           </div>
         </>
       )}
