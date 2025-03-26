abfee1d4c980a9026d9d4ad6645f72e96177d182
GeekNoob04 <harshitbudhraja0@gmail.com>
2025-03-26 11:18:03 +0530

    updated fav and fav slice

--- src/Components/Favourites.jsx
+++ src/Components/Favourites.jsx
@@ -22,16 +22,19 @@ const Favourites = () => {
     e.stopPropagation();
   };
 
-  const handleRemove = (index) => {
-    dispatch(removeFav(index));
+  // Remove by Movie ID instead of Index
+  const handleRemove = (movieId) => {
+    dispatch(removeFav(movieId));
   };
 
   const favMovies = useSelector((store) => store.favourites.favourites);
 
   return (
-    <div className="min-h-screen min-w-screen relative pt-[23%] md:pt-[8%] pb-14 ">
+    <div className="min-h-screen min-w-screen relative pt-[23%] md:pt-[8%] pb-14">
       <div className="absolute top-0 left-0 z-[-2] h-full w-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
-      <h1 className="text-white text-3xl text-center mb-10 md:mb-20">Favourites</h1>
+      <h1 className="text-white text-3xl text-center mb-10 md:mb-20">
+        Favourites
+      </h1>
 
       {favMovies.length === 0 ? (
         <div className="text-center text-white text-xl">
@@ -39,9 +42,9 @@ const Favourites = () => {
         </div>
       ) : (
         <div className="flex flex-wrap gap-4 pl-8 md:pl-12">
-          {favMovies.map((movie, index) => (
+          {favMovies.map((movie) => (
             <div
-              key={index}
+              key={movie.id} // Use movie.id as key
               className="md:w-52 md:h-70 w-36 h-42 md:mr-6 mr-3 flex-shrink-0 relative"
             >
               <>
@@ -53,7 +56,7 @@ const Favourites = () => {
                 />
                 <div
                   className="text-white text-4xl absolute top-0 -ml-1 opacity-85 cursor-pointer"
-                  onClick={() => handleRemove(index)}
+                  onClick={() => handleRemove(movie.id)}
                 >
                   <BsBookmarkDashFill />
                 </div>
@@ -104,7 +107,7 @@ const Favourites = () => {
                       {infoDiv.release_date || infoDiv.first_air_date}
                     </p>
                     <p className="my-4">{infoDiv.overview}</p>
-                    <p className="my-4 flex items-center ">
+                    <p className="my-4 flex items-center">
                       Rating: <FaStar className="text-yellow-500 ml-2 mr-1" />
                       {Math.round(infoDiv.vote_average * 10) / 10}/10
                     </p>
